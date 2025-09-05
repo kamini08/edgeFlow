d0938915dd4db20d5957666225a7f3d4f824775d
Sky Singh <akashsingh2210670@gmail.com>
2025-09-05 17:47:34 +0530

    test(frontend): switch to babel-jest with React+TS presets to compile TSX; remove ts-jest usage

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  preset: 'ts-jest',
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   transform: {
-    '^.+\\.(ts|tsx)$': [
-      'ts-jest',
-      { tsconfig: '<rootDir>/tsconfig.json', diagnostics: false, isolatedModules: true, tsconfigOverride: { compilerOptions: { jsx: 'react-jsx' } } },
-    ],
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
   },
 };
