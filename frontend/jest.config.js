0e92772771fc65d436e530689cf5a8bac55aed0d
Sky Singh <akashsingh2210670@gmail.com>
2025-09-05 17:36:02 +0530

    test(frontend): configure ts-jest transform for TSX to fix Jest parsing of JSX

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -4,4 +4,7 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
+  },
 };
