3d976857118a5b4f16be087aebe133dab9837dbb
Sky Singh <akashsingh2210670@gmail.com>
2025-09-05 17:26:20 +0530

    test: add backend integration tests and real frontend component tests (jest, ts-jest)

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
+  preset: 'ts-jest',
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 };
-
